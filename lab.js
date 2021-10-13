d8b7e89c564f3930b531ac82ed7f2d17453f1b73
LindsaySenia <lmzsenia@gmail.com>
2021-10-13 14:54:58 -0400

    Problem 10 solved

--- lab.js
+++ lab.js
@@ -151,7 +151,10 @@ console.log(changeMyMind)
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
 //CODE HERE
-
+if (changeMyMind !== true){
+  changeMyMind = true
+}
+console.log(changeMyMind)
 
 
 ////////// ADVANCED PROBLEMS //////////

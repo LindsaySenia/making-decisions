f60c3b7afe8d87481255fb599a2fc1fecb0eed64
LindsaySenia <lmzsenia@gmail.com>
2021-10-13 14:11:24 -0400

    Problem 3 solved

--- lab.js
+++ lab.js
@@ -43,7 +43,11 @@ if (amysAge > brittanisAge){
 */
 
 //CODE HERE
-
+if (amysBirthYear === brittanisBirthYear){
+  console.log('They were born in the same year.')
+} else {
+  console.log('They were not born in the same year.')
+}
 
 ////////// PROBLEM 4 //////////
 
